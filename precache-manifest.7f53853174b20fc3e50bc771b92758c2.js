6e858e8a93714e68fc5cc6ce4b014b67e7ee3a36
shummi7 <sharmila16moorthi@gmail.com>
2020-08-13 20:28:08 -0700

    Updates

---
+++ precache-manifest.7f53853174b20fc3e50bc771b92758c2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bbc3db8c337f73337522e8f432a7449",
+    "url": "/sportfolio/index.html"
+  },
+  {
+    "revision": "463b9d870d57fd3ceb5f",
+    "url": "/sportfolio/static/css/main.e7255156.chunk.css"
+  },
+  {
+    "revision": "d47c7060237f0f513d35",
+    "url": "/sportfolio/static/js/2.5d97f7f1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sportfolio/static/js/2.5d97f7f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463b9d870d57fd3ceb5f",
+    "url": "/sportfolio/static/js/main.31e8744a.chunk.js"
+  },
+  {
+    "revision": "fd9a3abf29ec02ad60e3",
+    "url": "/sportfolio/static/js/runtime-main.6191e338.js"
+  },
+  {
+    "revision": "fcba5155a54574592714dad175017fc2",
+    "url": "/sportfolio/static/media/about.fcba5155.svg"
+  },
+  {
+    "revision": "f56d1ed0fe05383c89f88fca536624d8",
+    "url": "/sportfolio/static/media/bg1.f56d1ed0.svg"
+  },
+  {
+    "revision": "181b0c314a0c8f2cd775951634330b4f",
+    "url": "/sportfolio/static/media/bg3.181b0c31.svg"
+  },
+  {
+    "revision": "21c74bc416812c83d38e7d19467a1276",
+    "url": "/sportfolio/static/media/bg4.21c74bc4.svg"
+  },
+  {
+    "revision": "1613396c00569b93a0dbd63be5305c3a",
+    "url": "/sportfolio/static/media/bg5.1613396c.svg"
+  },
+  {
+    "revision": "0c33747501dfd775408f9d29dc6c2496",
+    "url": "/sportfolio/static/media/bg55.0c337475.svg"
+  },
+  {
+    "revision": "84a13acd4f691a314c601262518432b8",
+    "url": "/sportfolio/static/media/bg6.84a13acd.svg"
+  },
+  {
+    "revision": "79a682330f952dcd6dacc7ca28256d19",
+    "url": "/sportfolio/static/media/bg7.79a68233.svg"
+  },
+  {
+    "revision": "4ab80b39d221fe7b92c9f3237b72ab53",
+    "url": "/sportfolio/static/media/brain.4ab80b39.svg"
+  },
+  {
+    "revision": "7b8f5d4e972d3e738207f0f7e3215be1",
+    "url": "/sportfolio/static/media/chatbot.7b8f5d4e.svg"
+  },
+  {
+    "revision": "c2789189ef9526a3ec32e828d04c61b9",
+    "url": "/sportfolio/static/media/color.c2789189.svg"
+  },
+  {
+    "revision": "63033bfd50f5dc45df4d82863961faf4",
+    "url": "/sportfolio/static/media/quiz.63033bfd.svg"
+  },
+  {
+    "revision": "e0564cc07136743bd83404032d8dddf5",
+    "url": "/sportfolio/static/media/shopping.e0564cc0.svg"
+  },
+  {
+    "revision": "4d4108ec2161103a749ae1bb857d87af",
+    "url": "/sportfolio/static/media/smoothie.4d4108ec.svg"
+  },
+  {
+    "revision": "8773b430eba8abb300f878795a9ea14d",
+    "url": "/sportfolio/static/media/snake.8773b430.svg"
+  },
+  {
+    "revision": "3911289d4b2b11834dbb4791c7332fd6",
+    "url": "/sportfolio/static/media/task.3911289d.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sportfolio/precache-manifest.7f53853174b20fc3e50bc771b92758c2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sportfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
